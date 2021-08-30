93ac3bee490387fee23c4851668f96e26e594156
Aaki阿湫 <mitian233@yahoo.co.jp>
2021-08-30 21:48:17 +0800

    Optimize JavaScript file's size

--- docs/translate.js
+++ docs/translate.js
@@ -1,67 +1,72 @@
+function textcontents(id, contents) {
+    document.getElementById(id).innerHTML = contents
+}
+
 function setjp() {
-    document.getElementById("pagetitle").innerHTML = "5000兆円ジェネレーター super"
-    document.getElementById("title").innerHTML = "5000兆円ジェネレーター super";
-    document.getElementById("subtitle").innerHTML = "5000兆円風の画像を生成するツールです。<span style=\"color: blue; \">`欲しい!`</span> も生成できます。";
-    document.getElementById("intro1").innerHTML = "これはGitHub上のオープンソースプロジェクトです"
-    document.getElementById("contributor2").innerHTML = "作者:<a href=\"https://twitter.com/yurafuca\">@yurafuca</a>，"
-    document.getElementById("contributor3").innerHTML = "原作者:<a href=\"https://twitter.com/rarity_rare25\">@rarity_rare25</a>（<a href=\"https://rare25.github.io/5000choyen/\">5000兆円ジェネレーター</a>）"
-    document.getElementById("changelog2").innerHTML = "<span style=\"color: blue; \">2021-08-29: ページの読み込みは速くなるため、Founderフォント代わりにGoogleフォントが使用されます。異常なフォント表示が発生した場合は、テキストを再入力してください。</span>"
-    document.getElementById("text1").innerHTML = "<span style=\"color: blue; \">`欲しい!`</span> の描画に"
-    document.getElementById("label1").innerHTML = "元ネタの画像を使う"
-    document.getElementById("label2").innerHTML = "文字を生成する"
-    document.getElementById("text2").innerHTML = "画像を保存するとき背景色を"
-    document.getElementById("label3").innerHTML = "透明にする"
-    document.getElementById("label4").innerHTML = "白にする"
-    document.getElementById("labelTop").innerHTML = "赤色の文字"
-    document.getElementById("labelBottom").innerHTML = "銀色の文字"
-    document.getElementById("savebutton").innerHTML = "画像を保存する"
-    document.getElementById("newtabbutton").innerHTML = "画像を新しいタブで開"
-    document.getElementById("ps").innerHTML = "`欲しい!` はドラッグできます。"
-    document.getElementById("source").innerHTML = "ソースコード:<a href=\"https://github.com/mitian233/5000choyen\">mitian233/5000choyen</a>"
+    textcontents("pagetitle", "5000兆円ジェネレーター super");
+    textcontents("title", "5000兆円ジェネレーター super");
+    textcontents("subtitle", "5000兆円風の画像を生成するツールです。<span style=\"color: blue; \">`欲しい!`</span> も生成できます。");
+    textcontents("intro1", "これはGitHub上のオープンソースプロジェクトです");
+    textcontents("contributor2", "作者:<a href=\"https://twitter.com/yurafuca\">@yurafuca</a>，");
+    textcontents("contributor3", "原作者:<a href=\"https://twitter.com/rarity_rare25\">@rarity_rare25</a>（<a href=\"https://rare25.github.io/5000choyen/\">5000兆円ジェネレーター</a>）");
+    textcontents("changelog2", "<span style=\"color: blue; \">2021-08-29: ページの読み込みは速くなるため、Founderフォント代わりにGoogleフォントが使用されます。異常なフォント表示が発生した場合は、テキストを再入力してください。</span>");
+    textcontents("text1", "<span style=\"color: blue; \">`欲しい!`</span> の描画に");
+    textcontents("label1", "元ネタの画像を使う");
+    textcontents("label2", "文字を生成する");
+    textcontents("text2", "画像を保存するとき背景色を");
+    textcontents("label3", "透明にする");
+    textcontents("label4", "白にする");
+    textcontents("labelTop", "赤色の文字");
+    textcontents("labelBottom", "銀色の文字");
+    textcontents("savebutton", "画像を保存する");
+    textcontents("newtabbutton", "画像を新しいタブで開");
+    textcontents("ps", "`欲しい!` はドラッグできます。");
+    textcontents("source", "ソースコード:<a href=\"https://github.com/mitian233/5000choyen\">mitian233/5000choyen</a>");
 
 }
 
 function setsc() {
-    document.getElementById("pagetitle").innerHTML = "5000兆円表情包生成器"
-    document.getElementById("title").innerHTML = "5000兆円表情包生成器";
-    document.getElementById("subtitle").innerHTML = "这是“5000兆円”风格的表情生成器。这个版本<span style=\"color: blue; \">`欲しい!`</span> 也可以生成。";
-    document.getElementById("intro1").innerHTML = "这是一个GitHub上开源的项目"
-    document.getElementById("contributor2").innerHTML = "作者:<a href=\"https://twitter.com/yurafuca\">@yurafuca</a>，"
-    document.getElementById("contributor3").innerHTML = "原作者:<a href=\"https://twitter.com/rarity_rare25\">@rarity_rare25</a>（<a href=\"https://rare25.github.io/5000choyen/\">5000兆円ジェネレーター</a>）"
-    document.getElementById("changelog2").innerHTML = "<span style=\"color: blue; \">2021-08-29: 由于加载速度过慢的问题，弃用方正字库而替换了谷歌字体（思源黑体和思源宋体）。若遇到字体显示不正常，请在文末反复增减一个空格以手动刷新canvas。</span>"
-    document.getElementById("text1").innerHTML = "在 <span style=\"color: blue; \">`欲しい!`</span> 的位置"
-    document.getElementById("label1").innerHTML = "不做更改"
-    document.getElementById("label2").innerHTML = "自行定义文字内容"
-    document.getElementById("text2").innerHTML = "设置保存图像的背景"
-    document.getElementById("label3").innerHTML = "透明色"
-    document.getElementById("label4").innerHTML = "白色"
-    document.getElementById("labelTop").innerHTML = "第一行文字（红色）"
-    document.getElementById("labelBottom").innerHTML = "第二行文字（白色）"
-    document.getElementById("savebutton").innerHTML = "保存图片"
-    document.getElementById("newtabbutton").innerHTML = "在新窗口打开图片"
-    document.getElementById("ps").innerHTML = "第二行文字是可以拖动的"
-    document.getElementById("source").innerHTML = "源码:<a href=\"https://github.com/mitian233/5000choyen\">mitian233/5000choyen</a>"
+    textcontents("pagetitle", "5000兆円表情包生成器");
+    textcontents("title", "5000兆円表情包生成器");
+    textcontents("subtitle", "这是“5000兆円”风格的表情生成器。这个版本<span style=\"color: blue; \">`欲しい!`</span> 也可以生成。");
+    textcontents("intro1", "这是一个GitHub上开源的项目");
+    textcontents("contributor2", "作者:<a href=\"https://twitter.com/yurafuca\">@yurafuca</a>，");
+    textcontents("contributor3", "原作者:<a href=\"https://twitter.com/rarity_rare25\">@rarity_rare25</a>（<a href=\"https://rare25.github.io/5000choyen/\">5000兆円ジェネレーター</a>）");
+    textcontents("changelog2", "<span style=\"color: blue; \">2021-08-29: 由于加载速度过慢的问题，弃用方正字库而替换了谷歌字体（思源黑体和思源宋体）。若遇到字体显示不正常，请在文末反复增减一个空格以手动刷新canvas。</span>");
+    textcontents("text1", "在 <span style=\"color: blue; \">`欲しい!`</span> 的位置");
+    textcontents("label1", "不做更改");
+    textcontents("label2", "自行定义文字内容");
+    textcontents("text2", "设置保存图像的背景");
+    textcontents("label3", "透明色");
+    textcontents("label4", "白色");
+    textcontents("labelTop", "第一行文字（红色）");
+    textcontents("labelBottom", "第二行文字（白色）");
+    textcontents("savebutton", "保存图片");
+    textcontents("newtabbutton", "在新窗口打开图片");
+    textcontents("ps", "第二行文字是可以拖动的");
+    textcontents("source", "源码:<a href=\"https://github.com/mitian233/5000choyen\">mitian233/5000choyen</a>");
 
 }
+
 function settc() {
-    document.getElementById("pagetitle").innerHTML = "5000兆円迷因製作工具";
-    document.getElementById("title").innerHTML = "5000兆円迷因製作工具";
-    document.getElementById("subtitle").innerHTML = "這是逆轉裁判風格的迷因製作工具。這個版本<span style=\"color: blue; \">`欲しい!`</span> 也可以製作。"
-    document.getElementById("intro1").innerHTML = "這是一個在GitHub上開源的項目"
-    document.getElementById("contributor2").innerHTML = "作者:<a href=\"https://twitter.com/yurafuca\">@yurafuca</a>，"
-    document.getElementById("contributor3").innerHTML = "原作者:<a href=\"https://twitter.com/rarity_rare25\">@rarity_rare25</a>（<a href=\"https://rare25.github.io/5000choyen/\">5000兆円ジェネレーター</a>）"
-    document.getElementById("changelog2").innerHTML = "<span style=\"color: blue; \">2021-08-29: 由於載入速度過慢的問題，棄用方正字形檔而替換了Google字體（思源黑體和思源宋體）。若遇到字體顯示不正常，請在文末反復增減一個空格以手動刷新canvas。</span>"
-    document.getElementById("text1").innerHTML = "在 <span style=\"color: blue; \">`欲しい!`</span> 的位置"
-    document.getElementById("label1").innerHTML = "不做更改"
-    document.getElementById("label2").innerHTML = "自定內容"
-    document.getElementById("text2").innerHTML = "設定保存的圖像檔案是否附帶背景"
-    document.getElementById("label3").innerHTML = "透明色"
-    document.getElementById("label4").innerHTML = "白色"
-    document.getElementById("labelTop").innerHTML = "第一行文字（红色）"
-    document.getElementById("labelBottom").innerHTML = "第二行文字（白色）"
-    document.getElementById("savebutton").innerHTML = "保存圖像"
-    document.getElementById("newtabbutton").innerHTML = "在新視窗裡打開圖片"
-    document.getElementById("ps").innerHTML = "第二行文字是可以拖動的"
-    document.getElementById("source").innerHTML = "原始程式碼:<a href=\"https://github.com/mitian233/5000choyen\">mitian233/5000choyen</a>"
+    textcontents("pagetitle", "5000兆円迷因製作工具");
+    textcontents("title", "5000兆円迷因製作工具");
+    textcontents("subtitle", "這是逆轉裁判風格的迷因製作工具。這個版本<span style=\"color: blue; \">`欲しい!`</span> 也可以製作。");
+    textcontents("intro1", "這是一個在GitHub上開源的項目");
+    textcontents("contributor2", "作者:<a href=\"https://twitter.com/yurafuca\">@yurafuca</a>，");
+    textcontents("contributor3", "原作者:<a href=\"https://twitter.com/rarity_rare25\">@rarity_rare25</a>（<a href=\"https://rare25.github.io/5000choyen/\">5000兆円ジェネレーター</a>）");
+    textcontents("changelog2", "<span style=\"color: blue; \">2021-08-29: 由於載入速度過慢的問題，棄用方正字形檔而替換了Google字體（思源黑體和思源宋體）。若遇到字體顯示不正常，請在文末反復增減一個空格以手動刷新canvas。</span>");
+    textcontents("text1", "在 <span style=\"color: blue; \">`欲しい!`</span> 的位置");
+    textcontents("label1", "不做更改");
+    textcontents("label2", "自定內容");
+    textcontents("text2", "設定保存的圖像檔案是否附帶背景");
+    textcontents("label3", "透明色");
+    textcontents("label4", "白色");
+    textcontents("labelTop", "第一行文字（红色）");
+    textcontents("labelBottom", "第二行文字（白色）");
+    textcontents("savebutton", "保存圖像");
+    textcontents("newtabbutton", "在新視窗裡打開圖片");
+    textcontents("ps", "第二行文字是可以拖動的");
+    textcontents("source", "原始程式碼:<a href=\"https://github.com/mitian233/5000choyen\">mitian233/5000choyen</a>");
 
 }
